8fb0ddc4886ed53e4adc1b6b744ec7ce23ade65b
OmeGak <omegak@gmail.com>
2013-06-09 19:18:07 +0200

    Template now fetches the ranking from redis

--- dict8or/views/core.py
+++ dict8or/views/core.py
@@ -1,4 +1,7 @@
+from collections import OrderedDict
+
 from flask import Blueprint, render_template
+from dict8or.app import redis
 
 
 core = Blueprint('core', __name__)
@@ -6,4 +9,13 @@ core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    return render_template('index.html')
+    ranking = _fetch_ranking()
+    return render_template('index.html', ranking=ranking)
+
+
+def _fetch_ranking():
+    ranking = OrderedDict()
+    packages = [p.decode('utf-8') for p in redis.zrangebyscore('ranking', '-inf', 'inf')]
+    for p in packages:
+        ranking[p] = {k.decode('utf-8'): v.decode('utf-8') for (k, v) in redis.hgetall(p).iteritems()}
+    return ranking
