2c28f0b28172af0e03fab40e241f631002c4c418
Pedro Ferreira <ilzogoiby@gmail.com>
2013-06-09 13:33:35 +0200

    Use Flask config for tasks

--- dict8or/tasks/pypi.py
+++ dict8or/tasks/pypi.py
@@ -4,22 +4,26 @@ import celery
 import requests
 import redis
 
+from dict8or.app import make_app
 
-PYPI_LIST_URL = "https://pypi.python.org/simple/"
+
 RE_PYPI_LIST_ENTRY = re.compile(r'<a href=[\'"](.*)[\'"]>([^<]+)</a>')
-DB_STORAGE = 'redis://localhost/0'
-
 
 @celery.task
 def fetch_pypi_list():
-    db = redis.Redis.from_url(DB_STORAGE)
+    app = make_app()
+    db = redis.Redis.from_url(app.config['DB_STORAGE'])
 
-    r = requests.get(PYPI_LIST_URL)
+    url = app.config['PYPI_LIST_URL']
+
+    r = requests.get(url)
     if r.status_code == 200:
-        list_packages = dict((e[1], PYPI_LIST_URL + e[0]) for e in re.findall(RE_PYPI_LIST_ENTRY, r.content))
+        list_packages = dict((e[1], url + e[0]) for e in re.findall(RE_PYPI_LIST_ENTRY, r.content))
     else:
         print("Impossible to fetch PyPI list: {}: {}".format(r.status_code, r.content))
 
     print("Packages retrieved")
+
+    db.delete('PYPI_PACKAGES')
     db.hmset('PYPI_PACKAGES', list_packages)
     print("Saved in DB")
