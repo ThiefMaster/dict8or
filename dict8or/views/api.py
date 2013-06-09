5924570e1d7b1541a2d9a6767e4c4bc802a09137
ThiefMaster <adrian@planetcoding.net>
2013-06-09 19:04:13 +0200

    Avoid race condition when enqueuing package

--- dict8or/views/api.py
+++ dict8or/views/api.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+
 from dict8or.app import redis
 from dict8or.tasks.pypi import fetch_and_check
 
@@ -17,10 +18,21 @@ def search_pypi_packages():
 @api.route('/enqueue-pypi-package', methods=('POST',))
 def enqueue_pypi_package():
     name = request.form['package']
-    if redis.sismember('pending_packages', name.lower()):
-        return jsonify(success=False, msg=u'Package is already queued for checking')
-    elif not redis.hexists('pypi_packages', name):
+    if not redis.hexists('pypi_packages', name):
         return jsonify(success=False, msg=u'Package does not exist')
-    redis.sadd('pending_packages', name.lower())
+    if _package_is_queued(name):
+        return jsonify(success=False, msg=u'Package is already queued for checking')
     fetch_and_check.delay(name)
     return jsonify(success=True)
+
+
+def _package_is_queued(package):
+    def _helper(pipe):
+        if pipe.sismember('pending_packages', package.lower()):
+            return True
+        pipe.multi()
+        pipe.sadd('pending_packages', package.lower())
+        return False
+
+    return redis.transaction(_helper, 'pending_packages', value_from_callable=True)
+
